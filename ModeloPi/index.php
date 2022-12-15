39960e06e1c782874fa388a080a2ed6efa4d474b
ArnaldoCarvalho <108664667+ArnaldoCarvalho@users.noreply.github.com>
2022-12-15 00:00:22 +0000

    Update modelo

---
+++ ModeloPi/index.php
@@ -0,0 +1,22 @@
+<?php
+  include 'php/codigo.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <title>Projeto</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php
+
+?>
+
+</body>
+</html>
